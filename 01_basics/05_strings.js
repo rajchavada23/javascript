499991a0a7e0a8c7fe1582878ffc90da1b45b74c
rajchavada23 <75298987+rajchavada23@users.noreply.github.com>
2024-02-23 12:52:04 +0000

    strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,34 @@
+const name = "myName"
+const repoCount = 50
+
+// console.log(name + repoCount + " value");
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String("Prince-DC-game");
+
+// console.log(gameName);
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('n'));
+
+const newString = gameName.substring(0, 4);
+console.log(newString);
+
+const anotherString = gameName.slice(-8, 4);
+console.log(anotherString);
+
+const newStringOne = "   Batman    "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "https://hitesh.com/hitesh%20choudhary"
+
+console.log(url.replace('%20', '-'));
+console.log(url.includes('hitesh'));
+console.log(url.includes('sweet'));
+
+console.log(gameName.split('-'));
