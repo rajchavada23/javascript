c618dd0e0e60f09c01461fd36b0b3bf18b078f7e
rajchavada23 <75298987+rajchavada23@users.noreply.github.com>
2024-02-26 14:12:52 +0000

    new changes on string and nums files

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -1,34 +1,40 @@
-const name = "myName"
+const name = "Robert"
 const repoCount = 50
 
-// console.log(name + repoCount + " value");
-// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+console.log(name + repoCount + " value"); // Robert50 value
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`); // Hello my name is Robert and my repo count is 50
 
 const gameName = new String("Prince-DC-game");
 
-// console.log(gameName);
-// console.log(gameName[0]);
-// console.log(gameName.__proto__);
+console.log(gameName); // [String: 'Prince-DC-game']
+console.log(gameName[0]); // P
+console.log(gameName.__proto__); // {}
 
-// console.log(gameName.length);
-// console.log(gameName.toUpperCase());
-// console.log(gameName.charAt(2));
-// console.log(gameName.indexOf('n'));
+console.log(gameName.length); // 14
+console.log(gameName.toUpperCase()); // PRINCE-DC-GAME
+console.log(gameName.charAt(2)); // i
+console.log(gameName.indexOf('n')); // 3
 
 const newString = gameName.substring(0, 4);
-console.log(newString);
+console.log(newString); // Prin
 
-const anotherString = gameName.slice(-8, 4); ///////////////
-console.log(anotherString);
+const anotherString = gameName.slice(1, 2);
+console.log('slice==>', anotherString);
 
-const newStringOne = "   Batman    "
-console.log(newStringOne);
-console.log(newStringOne.trim());
+const newStringOne = "   Batman   b "
+console.log(newStringOne); //     Batman    
+console.log(newStringOne.trim()); // Batman
+console.log(newStringOne.trimStart());
+console.log(newStringOne.trimEnd());
 
 const url = "https://hitesh.com/hitesh%20choudhary"
 
-console.log(url.replace('%20', '-'));
-console.log(url.includes('hitesh'));
-console.log(url.includes('sweet'));
+console.log(url.replace('%20', '-'));  // https://hitesh.com/hitesh-choudhary
+console.log(url.includes('hitesh')); // true
+console.log(url.includes('sweet')); // false
 
-console.log(gameName.split('-'));
+console.log(gameName.split('-')); // [ 'Prince', 'DC', 'game' ]
+
+const newGameName = new String('God of war');
+console.log(newGameName.split(' ')); // [ 'God', 'of', 'war' ]
+console.log(newGameName.split('o')); // [ 'G', 'd ', 'f war' ]
--- 01_basics/06_nums_and_math.js
+++ 01_basics/06_nums_and_math.js
@@ -1,37 +1,42 @@
 const score = 200
-// console.log(score);
+console.log(score); // 200
 
 const balance = new Number(100);
-// console.log(balance);
+console.log(balance); // [Number: 100]
 
-// console.log(balance.toString());
-// console.log(balance.toString().length);
-// console.log(balance.toFixed(2));
+console.log(balance.toString()); // 100
+console.log(balance.toString().length); // 3
+console.log(balance.toFixed(2)); // 100.00
 
 const otherNumber = 123.5896
 
-// console.log(otherNumber.toPrecision(4)); //////////////
+console.log(otherNumber.toPrecision(1)); // 1e+2
+console.log(otherNumber.toPrecision(2)); // 1.2e+2
+console.log(otherNumber.toPrecision(3)); // 124
+console.log(otherNumber.toPrecision(4)); // 123.6
+console.log(otherNumber.toPrecision(5)); // 123.59
+console.log(otherNumber.toPrecision(6)); // 123.590
 
 const hundreds = 1000000
 
-// console.log(hundreds.toLocaleString());
-// console.log(hundreds.toLocaleString('en-IN'));
+console.log(hundreds.toLocaleString()); // 1,000,000
+console.log(hundreds.toLocaleString('en-IN')); // 10,00,000
 
 // +++++++++++++ Maths +++++++++++++++++++++++++++++
 
-console.log(Math);
-console.log(Math.abs(-4));
-console.log(Math.round(4.5));
-console.log(Math.ceil(4.2));
-console.log(Math.floor(4.9));
-console.log(Math.min(4, 3, 6, 8));
-console.log(Math.max(4, 3, 6, 8));
+console.log(Math); // Object [Math] {}
+console.log(Math.abs(-4)); // 4
+console.log(Math.round(4.5)); // 5
+console.log(Math.ceil(4.2)); // 5
+console.log(Math.floor(4.9)); // 4
+console.log(Math.min(4, 3, 6, 8)); // 3
+console.log(Math.max(4, 3, 6, 8)); // 8
 
-console.log(Math.random());
-console.log((Math.random() * 10) + 1);
-console.log(Math.floor(Math.random() * 10) + 1);
+console.log(Math.random()); // 0.524561651
+console.log((Math.random() * 10) + 1); // 6.524561651
+console.log(Math.floor(Math.random() * 10) + 1); // 6
 
 const min = 10
 const max = 20
 
-console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+console.log(Math.floor(Math.random() * (max - min + 1)) + min); // 15
